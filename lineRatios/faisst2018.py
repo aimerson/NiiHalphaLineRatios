44a525d248c4529adee45ae5428609a0b26f2bd7
Alex Merson <alex.i.merson@gmail.com>
2018-02-16 22:14:07 -0800

    Revised implementation of Faisst et al. (2018) model.

--- lineRatios/faisst2018.py
+++ lineRatios/faisst2018.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 
+from .plotting.utils import *
 import sys
 import numpy as np
-from scipy.interpolate import RectBivariateSpline
+from scipy.interpolate import interp1d,spline
 
 class FaisstModel(object):
 
@@ -12,33 +13,42 @@ class FaisstModel(object):
         self.hubble = 0.7
         self.MASS = None
         return
+    
+    def FaisstEq3(self,N2,z):
+        xi = N2 + 0.138 - 0.042*((1.0+z)**2)
+        M = 3.696*xi + 3.236*(xi**-1) + 0.729*(xi**-2) + 14.928 + 0.156*((1.0+z)**2)    
+        return M
 
-    def buildNiiRedshiftInterpolator(self,gridSize=500,**kwargs):
+    def getStellarMass(self,niiRatio,redshift,hubble=None):                           
         funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name  
-        niiMin = -2.0
-        niiMax = -0.3
-        zMin = 0.0
-        zMax = 2.7
-        niiRange = np.linspace(niiMin,niiMax,gridSize)
-        zRange = np.linspace(zMin,zMax,gridSize)
-        ZZ,NII = np.meshgrid(zRange,niiRange)
-        xi = NII + 0.138 - 0.042*(1.0+ZZ)**2
-        MM = 3.696*xi + 3.236*(1.0/xi) + 0.729*(1.0/xi**2) + 14.928 + 0.156*(1.0+ZZ)**2
-        self.MASS = RectBivariateSpline(zRange,niiRange,MM,bbox=[zMin,zMax,niiMin,niiMax],**kwargs)
-        return
-
-    def getStellarMass(self,redshift,niiRatio,hubble=None,gridSize=500):
-        funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name  
-        zMask = np.logical_or(redshift<0.0,redshift>2.7)
-        if any(zMask):
-            raise ValueError(funcname+"(): one or more redshifts outside of allowed range 0 <= z <= 2.7.")
-        if self.MASS is None:
-            self.buildNiiRedshiftInterpolator(gridSize=gridSize)
-        mass = self.MASS.ev(redshift,niiRatio)
+        mass = np.array([self.FaisstEq3(nii,z) for nii,z in zip(niiRatio,redshift)])
         if hubble is not None:
             mass += np.log10(self.hubble/hubble)
         return mass
+
+    def interpolateN2Grid(self,redshift,mass):
+        NII = np.linspace(-1.5,-0.1,100)
+        Z = np.ones_like(NII)*redshift
+        MASS = self.getStellarMass(NII,Z)
+        f = interp1d(MASS,NII)
+        return f(mass)
+
+    def perturbNII(self,nii,z):
+        redshifts = [0.0,1.6,2.3]
+        errors = [0.13,0.21,0.22]
+        zz = np.linspace(0.0,2.3,1000)
+        err = spline(redshifts,errors,zz,order=2)
+        f = interp1d(zz,err,kind='linear',fill_value='extrapolate')
+        sigma = f(z)
+        return np.random.normal(loc=nii,scale=sigma)
     
+    def getN2(self,mass,redshift,hubble=None):
+        if hubble is not None:
+            mass += np.log10(hubble/self.hubble)
+        nii = np.array([self.interpolateN2Grid(z,m) for z,m in zip(redshift,mass)])
+        nii = self.perturbNII(nii,redshift)
+        return nii
 
-
-
+    def getNiiContamination(self,mass,redshift,hubble=None):
+        nii = 10.0**self.getN2(mass,redshift,hubble=hubble)
+        return nii/(nii+1.0)
